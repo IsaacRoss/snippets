abf9dc675ec2893b84127af3ef3f1c99e3dd0d52
Isaac Ross <isaac253@gmail.com>
2014-08-15 14:32:53 -0400

    readstream

---
+++ DataIO/Files/file_read_stream.js
@@ -0,0 +1,12 @@
+var fs = require('fs'),
+    options = {encoding:'utf8', flag: 'r'},
+    fileReadStream = fs.createReadStream('grains.txt', options);
+
+fileReadStream.on('data', function(chunk){
+    console.log("Grains: %s", chunk);
+    console.log("Read %d bytes of data.", chunk.length);
+});
+
+fileReadStream.on('close', function(){
+    console.log("File closed");
+});
