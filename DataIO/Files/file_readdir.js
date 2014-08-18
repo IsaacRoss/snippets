bdd3675a7b4486dede034b5ce963c6f45dcaa002
Isaac Ross <isaac253@gmail.com>
2014-08-18 17:39:55 -0400

    reading directories

---
+++ DataIO/Files/file_readdir.js
@@ -0,0 +1,23 @@
+var fs = require('fs'),
+    path = require('path');
+
+function WalkDirs(dirPath){
+    console.log(dirPath);
+    fs.readdir(dirPath, function(err, entries){
+        for(var idx in entries){
+            var fullPath = path.join(dirPath, entries[idx]);
+            (function(fullpath){
+                fs.stat(fullpath, function(err, stats){
+                    if(stats && stats.isFile()){
+                        console.log(fullpath);
+                    }else if(stats && stats.isDirectory()){
+                        WalkDirs(fullpath)
+                    }
+                })
+            })(fullPath);
+        }
+    });
+};
+
+
+WalkDirs('../../../');
